f3deaacbd91abd7c4e2a88d17197c08a5a32cfcf
jaynoedotart <jaynoedotart@gmail.com>
2026-06-27 21:09:28 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 let leaves = [];
 var c = document.getElementById("bgCanvas");
 var ctx = c.getContext("2d");
-c.width = window.innerWidth;
-c.height = window.innerHeight;
+c.width = window.outerWidth;
+c.height = window.outerHeight;
 
 class Leaf {
     constructor() {
+        this.t = 0;
+        this.X = 0;
+        this.Y = 0;
+        this.a = 0;
+        this.size = Math.random()*15 + 15;
+        this.xShift = Math.random()*screen.width;
+        this.yShift = -50;
+        this.xSwing = Math.random()*200 + 60;
+        this.ySwing = Math.random()*100 + 30;
+        this.speed = Math.random()*30 + 50;
         this.img = document.createElement("img");
         if(Math.random() < 0.2){
             this.img.src = "leaf1.png";
@@ -13,17 +23,6 @@ class Leaf {
         else {
             this.img.src = "leaf2.png";
         }
-        if(Math.random() < 0.5){
-            this.img.style.transform = "scaleX(-1)";
-        }
-        this.t = 0;
-        this.X = 0;
-        this.Y = 0;
-        this.xShift = Math.random()*screen.width;
-        this.yShift = -50;
-        this.xSwing = Math.random()*200 + 60;
-        this.ySwing = Math.random()*100 + 30;
-        this.speed = Math.random()*30 + 50;
     }
 }
 
@@ -40,9 +39,9 @@ function update(){
 function move(){
     for(let i = 0; i < leaves.length; i++){
         leaves[i].t++;
-        let t = leaves[i].t * 0.01;
-        leaves[i].X = leaves[i].xSwing * Math.cos(t) + leaves[i].xShift;
-        leaves[i].Y = leaves[i].ySwing * Math.sin(t) + leaves[i].speed * t + leaves[i].yShift;
+        leaves[i].a = leaves[i].t * 0.01;
+        leaves[i].X = leaves[i].xSwing * Math.cos(leaves[i].a) + leaves[i].xShift;
+        leaves[i].Y = leaves[i].ySwing * Math.sin(leaves[i].a) + leaves[i].speed * leaves[i].a + leaves[i].yShift;
         if(leaves[i].Y > screen.height + 400){
             leaves[i].img.remove();
             leaves.splice(i,1);
@@ -55,7 +54,16 @@ function move(){
 function render(){
     ctx.clearRect(0, 0, c.width, c.height);
     for(let i = 0; i < leaves.length; i++){
-        ctx.drawImage(leaves[i].img,leaves[i].X,leaves[i].Y,30,30);
+        const image = leaves[i].img;
+        let x = leaves[i].X;
+        let y = leaves[i].Y;
+        let width = image.size;
+        let height = image.size;
+
+        ctx.translate(x, y);
+        ctx.rotate(leaves[i].a);
+        ctx.drawImage(image, 0, 0, leaves[i].size, leaves[i].size);
+        ctx.rotate(-leaves[i].a);
+        ctx.translate(-x, -y);
     }
 }
-
